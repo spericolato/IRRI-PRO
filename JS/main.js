6b5ab2d05eeaa72ce2727700067017e0f1ea9f97
salhiabdennour <root@kali.dedsec>
2021-05-05 22:52:32 -0400

    update

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,6 @@ const menuIco = document.querySelector('.hamburger')
 
 const navBar = document.querySelector('.navbar')
 
-menuIco.addEventListener('click', () =>{
+menuIco.addEventListener('click', function(){
     navBar.classList.toggle('change');
 })
